08485dba1255c0e1716a8cbdf42fd0cb0703db55
user1 <62304291+Qrisno@users.noreply.github.com>
2021-01-20 18:08:03 +0400

    feat(added) added observables in one project with forms in anugalForm

--- angularForm/src/app/app.module.ts
+++ angularForm/src/app/app.module.ts
@@ -1,19 +1,23 @@
 import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {FormComponent} from './form/form.component';
 import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { CurrencyComponent } from './currency/currency.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    CurrencyComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
